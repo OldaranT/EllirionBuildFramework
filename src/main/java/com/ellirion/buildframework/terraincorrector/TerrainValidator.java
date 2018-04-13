0619263a537ddac167b6c6f9091f6f1860876606
Richard de Vries <riscool81@live.nl>
2018-04-13 16:28:49 +0200

    function setup again

--- src/main/java/com/ellirion/buildframework/terraincorrector/TerrainValidator.java
+++ src/main/java/com/ellirion/buildframework/terraincorrector/TerrainValidator.java
@@ -4,6 +4,11 @@ import com.ellirion.buildframework.model.BoundingBox;
 import org.bukkit.World;
 
 public class TerrainValidator {
+
+    private double overhangLimit = 50;
+    private double blocksLimit = 300;
+    private double totalLimit = 400;
+
     /***
      *
      * @param boundingBox bla
@@ -16,7 +21,21 @@ public class TerrainValidator {
             return Double.POSITIVE_INFINITY;
         }
 
-        if
+        if (calculateOverhang(boundingBox, world) > overhangLimit) {
+            return Double.POSITIVE_INFINITY;
+        }
+
+        if (calculateBlocks(boundingBox, world) > blocksLimit) {
+            return Double.POSITIVE_INFINITY;
+        }
+
+        final Double total = calculateBlocks(boundingBox, world) + calculateOverhang(boundingBox, world);
+
+        if (total > totalLimit) {
+            return Double.POSITIVE_INFINITY;
+        }
+
+        return total;
     }
 
     private double calculateOverhang(final BoundingBox boundingBox, final World world) {
