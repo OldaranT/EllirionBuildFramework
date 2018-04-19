32f8fc1bea88de9aa643fecca271ae09840abdd8
OldaranT <elliriongames@gmail.com>
2018-04-19 13:46:48 +0200

    Made code more modulair and clean up.

--- src/main/java/com/ellirion/buildframework/templateengine/model/TemplateBlock.java
+++ src/main/java/com/ellirion/buildframework/templateengine/model/TemplateBlock.java
@@ -6,14 +6,11 @@ import lombok.experimental.Accessors;
 import net.minecraft.server.v1_12_R1.NBTTagCompound;
 import org.bukkit.Material;
 import org.bukkit.material.MaterialData;
-
 import javax.annotation.Nullable;
-import java.util.LinkedList;
-import java.util.List;
 
 public class TemplateBlock {
     /**
-     * The actual block.
+     * The material of the block.
      */
     @Getter
     @Setter
@@ -24,12 +21,6 @@ public class TemplateBlock {
      */
     @Accessors
     private int templateBlockID;
-
-    /**
-     * All markers assigned to this block.
-     */
-    @Accessors
-    private List<String> markers;
 
     /**
      * If the block has metadata, we can store it here.
@@ -49,16 +40,5 @@ public class TemplateBlock {
      */
     public TemplateBlock(final Material material) {
         this.material = material;
-
-        markers = new LinkedList<String>();
-    }
-
-    /**
-     *
-     * @param marker The marker to add to this block
-     * @return whether the marker was added
-     */
-    protected boolean addMarker(final String marker) {
-        return markers.add(marker);
     }
 }
