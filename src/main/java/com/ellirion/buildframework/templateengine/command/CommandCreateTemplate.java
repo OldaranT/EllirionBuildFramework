811317875797fe6a811829b507ed1ea3a15d3b95
Chris Coerdes <chris.coerdes@gmail.com>
2018-04-20 11:17:01 +0200

    Removed unused import

--- src/main/java/com/ellirion/buildframework/templateengine/command/CommandCreateTemplate.java
+++ src/main/java/com/ellirion/buildframework/templateengine/command/CommandCreateTemplate.java
@@ -11,8 +11,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Arrays;
-
 public class CommandCreateTemplate implements CommandExecutor {
 
     @Override
