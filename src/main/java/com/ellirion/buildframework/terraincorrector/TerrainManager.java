98bd6b4db24ee82af27a41b6b235ab5d9412059a
Richard de Vries <riscool81@live.nl>
2018-04-25 10:14:08 +0200

    added bounding box validation to validator

---
+++ src/main/java/com/ellirion/buildframework/terraincorrector/TerrainManager.java
@@ -0,0 +1,12 @@
+package com.ellirion.buildframework.terraincorrector;
+
+import lombok.Getter;
+import com.ellirion.buildframework.model.BoundingBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TerrainManager {
+
+    @Getter private static List<BoundingBox> BoundingBoxes = new ArrayList<>();
+}
