c2997419866decb64891f47198f2c42bb8b2bc33
Richard de Vries <riscool81@live.nl>
2018-04-25 09:15:05 +0200

    initial setup

---
+++ src/main/java/com/ellirion/buildframework/terraincorrector/TerrainCorrector.java
@@ -0,0 +1,14 @@
+package com.ellirion.buildframework.terraincorrector;
+
+import com.ellirion.buildframework.model.BoundingBox;
+
+public class TerrainCorrector {
+
+    /**
+     * @param boundingBox the BoundingBox that will be used for terrain smoothing
+     * @return whether the smoothing succeeded
+     */
+    public boolean correctTerain(BoundingBox boundingBox) {
+        return true;
+    }
+}
---
+++ src/main/java/com/ellirion/buildframework/terraincorrector/command/CorrectCommand.java
@@ -0,0 +1,34 @@
+package com.ellirion.buildframework.terraincorrector.command;
+
+import com.sk89q.worldedit.bukkit.selections.CuboidSelection;
+import com.sk89q.worldedit.bukkit.selections.Selection;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import com.ellirion.buildframework.util.WorldEditHelper;
+
+public class CorrectCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (!(commandSender instanceof Player)) {
+            return false;
+        }
+
+        Player player = (Player) commandSender;
+
+        Selection sel = WorldEditHelper.getSelection(player);
+
+        if (!(sel instanceof CuboidSelection)) {
+            player.sendMessage(ChatColor.DARK_RED + "Invalid Selection!");
+            return false;
+        }
+
+        CuboidSelection selection = (CuboidSelection) sel;
+        
+
+        return false;
+    }
+}
