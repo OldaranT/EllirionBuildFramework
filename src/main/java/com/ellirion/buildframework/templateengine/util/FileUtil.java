1a55a6cf5f3359ea6007e3cd0547b94010eb0bea
OldaranT <elliriongames@gmail.com>
2018-05-23 12:05:51 +0200

    Better check on nbt files.

--- src/main/java/com/ellirion/buildframework/templateengine/util/FileUtil.java
+++ src/main/java/com/ellirion/buildframework/templateengine/util/FileUtil.java
@@ -1,5 +1,6 @@
 package com.ellirion.buildframework.templateengine.util;
 
+import org.apache.commons.io.FilenameUtils;
 import com.ellirion.buildframework.BuildFramework;
 
 import java.io.File;
@@ -21,7 +22,7 @@ public class FileUtil {
         List<String> listOfFileNames = new ArrayList<>();
         if (listOfFiles != null) {
             for (File f : listOfFiles) {
-                if (f.isFile() && f.getName().contains(".nbt")) {
+                if (f.isFile() && FilenameUtils.getExtension(f.getName()).equals("nbt")) {
                     listOfFileNames.add(f.getName().substring(0, f.getName().length() - 4).toUpperCase());
                 }
             }
